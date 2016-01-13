8b4957447a8b17e95d569e13305946bf39c893e0
Elder Lucas <elderlucas@gmail.com>
2016-01-13 18:49:57 -0200

    Controle do KpacitorPi Robo via WebBrowser
    - Foram adicionados os arquivos que controlam via web browser todos os
    movimentos do Robo

--- basic_run_kpacitorpi/run_kapacitorpi_keyboard.py
+++ basic_run_kpacitorpi/run_kapacitorpi_keyboard.py
@@ -6,9 +6,9 @@
 #/###################################################################
 #/ Author: Elder Lucas de Oliveira Santos							 				
 #/ Data: 13 de Janeiro de 2015										 
-#/ Titulo: Robo KapacitorPi											 
+#/ Titulo: Comandando o Robo com Teclado									 
 #/###################################################################
-#/ Arquivo: run_kapacitorpi.py 									     
+#/ Arquivo: run_kapacitorpi_keyboard.py 									     
 #/ Descricao: Controle das funcoes basicas do Frente, Tras, Esquerda 
 #/			  e direita 											 
 #/																	 
@@ -21,10 +21,23 @@ import RPi.GPIO as GPIO
 import time
 import os
 
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(22,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+
+dir_frente	= GPIO.PWM(17, 100) #Direita Para Frente
+dir_tras	= GPIO.PWM(22, 100) #Direita Para Tras
+esq_frente	= GPIO.PWM(23, 100) #Esquerda Para Frente
+esc_tras	= GPIO.PWM(27, 100) #Esquerda Para Tras 
+
 def movimentar(run):
 	if run == 'FRENTE':
-		dir_frente.start(20) #Duty Cycle de 20%
-		esq_frente.start(18) #Duty Cycle de 20%
+		dir_frente.start(19) #Duty Cycle de 20%
+		esq_frente.start(20) #Duty Cycle de 20%
 		dir_tras.stop()
 		esc_tras.stop()
 
@@ -60,32 +73,28 @@ def delay_info(time):
 		time.sleep(1)
 		print 'Time', x
 
+while True:
+	n = raw_input(" f(Frente) t(Tras) e(Esquerda) d(Direita) : ")
+	if n.strip() == 'f':
+		print 'Para frente'
+		movimentar('FRENTE')
+	elif n.strip() == 't':
+		print 'Para Tras'
+		movimentar('TRAS')
+	elif n.strip() == 'e':
+		print 'Para Esquerda'
+		movimentar('ESQUERDA')
+	elif n.strip() == 'd':
+		print 'Para Direita'
+		movimentar('DIREITA')
 
-GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(22,GPIO.OUT)
-GPIO.setup(23,GPIO.OUT)
-GPIO.setup(27,GPIO.OUT)
-
-dir_frente	= GPIO.PWM(17, 100) #Direita Para Frente
-dir_tras	= GPIO.PWM(22, 100) #Direita Para Tras
-esq_frente	= GPIO.PWM(23, 100) #Esquerda Para Frente
-esc_tras	= GPIO.PWM(27, 100) #Esquerda Para Tras 
-
-movimentar(run = 'FRENTE')
-delay_info(3)
-movimentar(run = 'TRAS')
-delay_info(3)	
-movimentar(run = 'ESQUERDA')
-delay_info(3)
-movimentar(run = 'DIREITA')
-delay_info(3)
-movimentar(run = 'STOP')
-
+	elif n.strip() == '9':
+		print 'Volte Sempre'
+		movimentar('Stop')
+		break
+	else:
+		print 'Esse Comando nao Existe'
 
 
-
-
-
+GPIO.cleanup()
 
